3a541cc1fc81b9b454adbfb7aca8e2d3acda8296
Mathew Ling <m.ling@deakin.edu.au>
2018-04-01 19:46:38 +1000

    Fixes the rworldmap demonstration in the countries vignette

--- vignettes/Countries.R
+++ vignettes/Countries.R
@@ -1,4 +1,4 @@
-## ------------------------------------------------------------------------
+## ----include=FALSE-------------------------------------------------------
 library(dplyr)
 library(magrittr)
 library(Qualtrics)
@@ -13,13 +13,23 @@ countries <- countries
 a <-  a %>% left_join(countries, by = "qualtrics")
 CreateTableOne("country", data = a)
 
-## ---- fig.show='hold'----------------------------------------------------
-b <- joinCountryData2Map(a, joinCode = "ISO3", nameJoinColumn = "alpha_3")
-#par(mai=c(0,0,0.2,0),xaxs="i",yaxs="i")
-#mapBubbles(dF=b,
-#           oceanCol="lightblue",
-#           landCol="wheat")
-
 ## ------------------------------------------------------------------------
-CreateTableOne("region", data = a)
+# obtain the frequency of each country
+freq <- a %>% group_by(country) %>% count() 
+a <- a %>% left_join(freq)
+
+# bind to country data
+b <- joinCountryData2Map(a, joinCode = "ISO3", nameJoinColumn = "alpha_3")
+
+# plot graph
+par(mai=c(0,0,0.2,0),xaxs="i",yaxs="i")
+mapBubbles(dF=b,
+           nameZSize="n",
+           nameZColour = "region",
+           oceanCol="lightblue",
+           landCol="wheat")
+
+## ---- fig.show='hold'----------------------------------------------------
+CreateTableOne("sub_region", data = a)
+ggplot(a, aes(region)) + geom_bar()
 
